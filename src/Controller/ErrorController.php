e8c888bc4d2655918b9baed235391e93b76df22e
ghign0 <m.ghinassi@carplanner.com>
2018-03-23 02:00:15 +0100

    added function list not found

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -9,12 +9,17 @@ class ErrorController extends SymfonyController
 {
     public function error403()
     {
-        return $this->render('mainTemplate/errors/403.html.twig', []);
+        return $this->render('@template/errors/403.html.twig', []);
     }
 
     public function error404()
     {
-        return $this->render('mainTemplate/errors/404.html.twig', []);
+        return $this->render('@template/errors/404.html.twig', []);
+    }
+
+    public function listNotfoundError ()
+    {
+        return $this->render('@template/errors/list-not-found.html.twig', []);
     }
 
 }
