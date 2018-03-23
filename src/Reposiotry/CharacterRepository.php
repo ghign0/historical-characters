c368abc55beffb7b118088b9bfc1aba356de3a0c
ghign0 <m.ghinassi@carplanner.com>
2018-03-23 02:01:08 +0100

    added dispatcher and new functions

--- src/Reposiotry/CharacterRepository.php
+++ src/Reposiotry/CharacterRepository.php
@@ -2,23 +2,35 @@
 
 namespace App\Reposiotry;
 
+use App\Event\SanitizeDataAfetrLoadEvent;
+use App\Exception\EmptyListExcpetion;
 use App\Model\Entity\Character;
-
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
 class CharacterRepository
 {
     const ROOT_DATA_DIR = __DIR__.'/../../data';
 
     private $direcotryHandler;
 
-    public function __construct()
+    private $dispatcher;
+
+    /**
+     * CharacterRepository constructor.
+     * @param EventDispatcher $dispatcher
+     */
+    public function __construct(EventDispatcher $dispatcher)
     {
         $this->direcotryHandler = opendir(self::ROOT_DATA_DIR);
+        $this->dispatcher = $dispatcher;
     }
 
     /**
+     * Resittuiscce la lista dei nomi
+     *
      * @return array
      */
-    public function getCharactersList() : array
+    public function getCharactersNameList() : array
     {
         $lsitOfCharacters = array();
         while( false !== $characterFile = readdir($this->direcotryHandler)) {
@@ -31,30 +43,133 @@ class CharacterRepository
     }
 
 
-    public function getAllCharacters()
+    /**
+     * Restituisce diversi tipi di liste di characters
+     *
+     * all                          - tutti
+     * hompegae                     - tutti quelli che hanno isForHompage() = true
+     * random [ 'random-number' ]   - un numero causale di Characters
+     * anniversary                  - i Characters nati nello stesso giorno e mese di oggi
+     * year ['year']                - tutti quelli di un determinato anno
+     * century ['century']          - tutti quelli di un determinato secolo
+     *
+     * @param string $typeOfList
+     * @param array $parameters
+     * @return array
+     */
+    public function getCharacters( string $typeOfList, array $parameters) : array
     {
+        $characters = array();
+        $listOfCharacters = $this->getCharactersNameList();
+        switch ($typeOfList) {
+            case 'all':
+                $characters = $this->getAllCharacters($listOfCharacters);
+                break;
 
+            case 'homepage' :
+                $characters = $this->getCharactersForHomepgae($listOfCharacters);
+                break;
+            case 'anniversary' :
+                $characters = $this->getCharactersOfTheDay($listOfCharacters);
+                break;
+
+            case 'random':
+                $characters = $this->getRandomCharacters( $listOfCharacters, $parameters['random-number'] );
+                break;
+
+            default:
+                $characters = $this->getAllCharacters($listOfCharacters);
+        }
+
+        return $characters;
     }
 
+
+    /**
+     * Restituisce il character dal nome
+     *
+     * @param string $slug
+     * @return Character
+     */
     public function getCharacterByFilename( string $slug) : Character
     {
         $characterFile = self::ROOT_DATA_DIR.'/'.$slug.'.json';
         $characterRawData = json_decode(file_get_contents($characterFile));
-        
-        return Character::createFromJson($characterRawData);
+
+        $character = Character::createFromJson($characterRawData);
+
+        /**  Pulisce il testo   */
+        $this->dispatcher->dispatch(SanitizeDataAfetrLoadEvent::NAME , new SanitizeDataAfetrLoadEvent($character));
+
+        return $character;
     }
 
+    /**
+     * Restituisce tutti i Characters
+     *
+     * @param $listOfCharacters
+     * @return array
+     */
+    private function getAllCharacters(array $listOfCharacters)  : ?array
+    {
+        var_dump($listOfCharacters);
+        foreach( $listOfCharacters as $characterFilename) {
+            $characters[] = $this->getCharacterByFilename($characterFilename);
+            }
+        return $characters;
+    }
 
-    public function getCharactersForHomepgae()
+    /**
+     * Restituisce lista di Character per la homepage
+     *
+     * @return array
+     */
+    private function getCharactersForHomepgae(array $listOfCharacters) : array
+    {
+
+        foreach( $listOfCharacters as $characterFilename) {
+            $characters[] = $this->getCharacterByFilename($characterFilename);
+        }
+
+        return $characters;
+    }
+
+    private function getCharactersOfTheDay(array $listOfCharacters ): array
     {
         $characters = array();
-        $lsitOfCharacters = $this->getCharactersList();
-
-        foreach( $lsitOfCharacters as $characterFilename) {
-
+        $listOfCharacters = $this->getCharactersNameList();
+        foreach( $listOfCharacters as $characterFilename) {
             $characters[] = $this->getCharacterByFilename($characterFilename);
-
         }
         return $characters;
     }
+
+    private function getRandomCharacters( array $listOfCharacters , int $randomNumer ): array
+    {
+        $characters = array();
+        $listOfCharacters = $this->getCharactersNameList();
+        shuffle( $listOfCharacters);
+        $listOfCharacters = array_slice($listOfCharacters, 0, $randomNumer );
+
+        foreach( $listOfCharacters as $characterFilename) {
+            $characters[] = $this->getCharacterByFilename($characterFilename);
+        }
+        return $characters;
+
+    }
+
+    private function getCharactersOfTheYear(array $listOfCharacters) : array
+    {
+        $characters = array();
+
+        return $characters;
+    }
+
+    private function getCharactersOfTheCentury(array $listOfCharacters) : array
+    {
+        $characters = array();
+
+        return $characters;
+    }
+
 }
